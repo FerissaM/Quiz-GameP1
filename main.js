6907bb45039f40e075f7aff0a32657231eb8cb5a
FerissaM <mabalotferissa@gmail.com>
2024-03-28 10:28:46 -0700

    pseudocode

---
+++ main.js
@@ -0,0 +1,17 @@
+// Pseudocode
+
+/* add start button to start the game */
+/* player should be able to see how many wrong
+ guess they can make (3 hearts) */
+/* question and possible answers (4 options) 
+ in an array */
+/* If answer is correct, the button will turn green.
+ If wrong, red */
+/* show the right answer when the player 
+ guessed wrong */
+/* put next button to go to the next question */
+/* track win and lose logic */
+/* release score after playing the game */
+/* play again button (reset function) */
+/* make play again button invisible when
+ game starts again */
